210f37e1639be3307c24c1cafca3cac7da7b590a
burak <burak@linux.com>
2019-10-24 18:41:54 +0200

    dashes

--- internal/config/parser.go
+++ internal/config/parser.go
@@ -2,16 +2,19 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"strings"
 )
+
+var supportedAlgs string = "MD2,MD4,MD5,GOST,SHA1,SHA224,SHA256,SHA384,SHA512,RMD160,SHA3-224,SHA3-256,SHA3-384,SHA3-512,SHA512-224,SHA512-256,BLAKE2s256,BLAKE2b256,BLAKE2b384,BLAKE2b512,SHAKE128,SHAKE256,SM3"
 
 func ParseConfig() *Config {
 	c := new(Config)
 	var algo *string
-	c.LocalOnly = flag.Bool("localOnly", false, "Only calculate checksum locally if remote servers present in config it will use those servers also local resources as well")
-	c.LocalOnly = flag.Bool("l", false, "Only calculate checksum locally, if remote servers present in config it will use those servers also local resources as well")
-	algo = flag.String("algo", "SHA512", "Checksum algorithm, supported: {MD5,SHA-0,SHA-1,SHA256,SHA384,SHA512,SHA-3}")
-	algo = flag.String("a", "SHA512", "Checksum algorithm, supported: {MD5,SHA-0,SHA-1,SHA256,SHA384,SHA512,SHA-3}")
+	c.LocalOnly = flag.Bool("localOnly", false, "Only calculate checksum locally \nif remote servers present in config app will ignore those servers")
+	c.LocalOnly = flag.Bool("l", false, "Only calculate checksum locally \nif remote servers present in config app will ignore those servers")
+	algo = flag.String("algo", "SHA512", fmt.Sprintf("Checksum algorithm, you can choose multiple by using MD5,SHA512... \nsupported algos: %s", supportedAlgs))
+	algo = flag.String("a", "SHA512", fmt.Sprintf("Checksum algorithm, you can choose multiple by using MD5,SHA512... \nsupported algos: %s", supportedAlgs))
 	c.LogLevel = flag.String("logLevel", "WARNING", "log level, supported: {TRACE,DEBUG,INFO,WARNING,ERROR}")
 	c.Proxy = flag.String("proxy", "", "Proxy address to reach file or servers")
 	c.Proxy = flag.String("p", "", "Proxy address to reach file or servers")
@@ -26,13 +29,15 @@ func ParseConfig() *Config {
 	c.All = flag.Bool("all", false, "Run all algorithms (MD5,SHA1 , SHA256 ...) for each running client")
 	c.Key = flag.String("key", "", "Key for blake2 hashing")
 	c.Key = flag.String("k", "", "Key for blake2 hashing")
-	c.Supplier = flag.String("supplier", "go", "Algorithm supplier default app (GO) libraries used, if you want to use unix, win, mac default apps set to OS, cloud providers support may vary")
-	c.Supplier = flag.String("s", "go", "Algorithm supplier default app (GO) libraries used, if you want to use unix, win, mac default apps set to OS, cloud providers support may vary")
+	c.Supplier = flag.String("supplier", "go", "Algorithm supplier default is [GO] that core golang libraries used, if you want to use unix, win, mac default apps set to [OS], for openssl set [openssl] cloud providers support may vary")
+	c.Supplier = flag.String("s", "go", "Algorithm supplier default is [GO] that core golang libraries used, if you want to use unix, win, mac default apps set to [OS], for openssl set [openssl] cloud providers support may vary")
 	var empty string = ""
 	c.File = &empty
 	c.Cheksum = &empty
 
 	flag.Parse()
+	lower := strings.ToLower(*c.Supplier)
+	c.Supplier = &lower
 	c.Algorithm = strings.Split(*algo, ",")
 	args := flag.Args()
 	if args != nil {
