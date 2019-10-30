34964a01999e6eba7b738c36a0475c7dc44aebfe
burak <burak@linux.com>
2019-10-30 13:32:26 +0100

    main comments

--- cmd/getsum/main.go
+++ cmd/getsum/main.go
@@ -1,4 +1,4 @@
-// Package main provides ...
+// Main package to handle configuration, parsing, validation and calculate checksums
 package main
 
 import (
@@ -15,22 +15,28 @@ import (
 )
 
 func main() {
+	//read command line params as well as parse user config if exist
 	config, err := parser.ParseConfig()
 	if err != nil {
 		logger.Error("Can not parse configuration: %s", err.Error())
 		os.Exit(1)
 	}
 
+	//if all good validate configuration
 	err = validator.ValidateConfig(config, false)
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
 
+	//if user defined set log level
 	logger.SetLevel(*config.LogLevel)
 	logger.Debug("Application  started, using config %v", *config)
+
+	//check if user wants to run in listen mode
 	if *config.Serve {
 		logger.Warn("Running in server mode listen address %s , port: %d", *config.Listen, *config.Port)
+		//onpremise is default server reads the config and listens given address and port no interface support atm
 		server := &servers.OnPremiseServer{}
 		err := server.Start(config)
 		if err != nil {
@@ -39,6 +45,9 @@ func main() {
 		os.Exit(1)
 	}
 
+	//providers are listeners of running processes
+	//currently we have os,openssl and go
+	//in future there might be aws,get etc... cloud function listeners
 	logger.Trace("Collecting providers")
 	var factory IProviderFactory = new(ProviderFactory)
 	providers, err := factory.GetProviders(config)
@@ -55,7 +64,10 @@ func main() {
 
 	logger.Trace("Starting to watch running processes")
 
+	//print the header
 	logger.Header(providers)
+
+	//if user specified a checksum to validate run in validation mode
 	if providers.HasValidation && providers.HasRemote && providers.HasLocal {
 		runRemoteFirst(providers, config)
 	} else {
@@ -63,53 +75,84 @@ func main() {
 	}
 	logger.Debug("Application finish")
 
+	//if error occured notify user
 	if providers.HasError() {
 		os.Exit(1)
 	}
 
 }
 
+//simply loops until no running process found
+//definition of running is routine status set to >= COMPLETED
 func watch(providers *Providers, config *parser.Config) {
 	for providers.IsRunning() {
 		logger.Status(providers)
+		//TODO: In case of remote http request set every 200ms
+		//This might be needed to increase maybe to lower net load
 		time.Sleep(200 * time.Millisecond)
 	}
+	//one final time print statuses
 	logger.Status(providers)
 }
 
+//if user provided a validation checksum
+// 1- check if any process completed
+// 2- check if calculated sum matches
+// 3- check if its matches if yes set status VALIDATED otherwise MISMATCH
 func checkMismatch(providers *Providers, config *parser.Config) {
 	if providers.HasMismatch(*config.Cheksum) {
 		logger.Debug("There are mismatches")
 		logger.Status(providers)
+		//print results
 		logger.Logsum(providers)
+		//check if user still wants to keep the file
 		if !*config.Keep {
 			logger.Debug("Exiting application no keep setted")
 			providers.Delete()
 			os.Exit(1)
 		}
 	} else {
+		//all good print one last time statuses
 		logger.Status(providers)
 	}
 
 }
 
+//this function runs all local/remote routines together
+//disregarding user wants validation or not
+//which means file still downloaded locally
+//yet still if user dont set -keep param
+//file will be deleted in case of validation present
 func runAll(providers *Providers, config *parser.Config) {
 	logger.Debug("Running all providers validation")
 	providers.Run()
 	watch(providers, config)
+	//all process finish notify runners
+	//to terminate their processes
 	providers.Terminate(true)
 	checkMismatch(providers, config)
+	//print results
 	logger.Logsum(providers)
 
 }
 
+//First runs remote runners
+//which will reach servers and collect checksums
+//if mismatch prevents host pc to fetch the file
+//otherwise local will run after remote validation
+//host will download the file and if mismatch
+//will keep the file according to -keep param
 func runRemoteFirst(providers *Providers, config *parser.Config) {
 	logger.Debug("Running remote providers")
+	//local waitgroup will be in wait mode
 	providers.SuspendLocals()
 	providers.Run()
 	watch(providers, config)
+	//terminates only finished processes
 	providers.Terminate(false)
 	checkMismatch(providers, config)
+	//if there is mismatch os.Exit(1)
+	//so no need to worry about suspended runners
 	logger.Debug("Running local providers")
 	providers.ResumeLocals()
 	watch(providers, config)
@@ -118,6 +161,8 @@ func runRemoteFirst(providers *Providers, config *parser.Config) {
 	logger.Logsum(providers)
 }
 
+//in case of user in hurry or terminated
+//make sure no runners
 func handleExit(providers *Providers) {
 	sign := make(chan os.Signal, 1)
 	signal.Notify(sign, os.Interrupt)
