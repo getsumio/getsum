cd263192a071ab38a633d7f10a125bdb0e328ef5
burak <burak@linux.com>
2019-10-31 16:18:59 +0100

    gen script update

--- generateReleases.sh
+++ generateReleases.sh
@@ -7,7 +7,10 @@ for GOOS in darwin linux windows openbsd freebsd ; do
         if [[ "$GOOS" == "windows" ]];then
           EXTENSION=".exe"
         fi
-        time GOOS=$GOOS GOARCH=$GOARCH go build -v -o ./$GO_BIN_PATH/getsum$EXTENSION ./cmd/getsum/main.go 
-        tar -czvf ./$GO_BIN_PATH/getsum-$GOOS-$GOARCH.tar.gz ./$GO_BIN_PATH/
+        time GOOS=$GOOS GOARCH=$GOARCH go build -a -v -o ./$GO_BIN_PATH/getsum$EXTENSION ./cmd/getsum/main.go 
+        TAR_FILE=$(pwd)/$GO_BIN_PATH/getsum-$GOOS-$GOARCH.tar.gz
+        tar -czvf $TAR_FILE $GO_BIN_PATH
     done
 done
+
+tree -h builds
