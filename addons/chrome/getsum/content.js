ed6e0f862a2ccd3929efca8dadcb90af6245f78b
burak <burak@linux.com>
2019-11-04 00:09:45 +0100

    base post

--- addons/chrome/getsum/content.js
+++ addons/chrome/getsum/content.js
@@ -1,24 +1,37 @@
 var $j = jQuery.noConflict();
-chrome.runtime.onMessage
-		.addListener(function(request, sender, sendResponse) {
-			if (request.name == "getsum") {
-				config = request.config;
-				if (config.checksum == "Y") {
-					config.checksum = window.getSelection().toString();
-				}else {
-					config.checksum = "";
-				}
-				var cfg = JSON.stringify(config);
-				
-				$j('#getsumModalDialog').toggle("slide:right");
-				console.log('done');
-			}
-		});
+var processId;
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.name == "getsum") {
+		config = request.config;
+		if (config.checksum == "Y") {
+			config.checksum = window.getSelection().toString();
+		} else {
+			config.checksum = "";
+		}
+		var cfg = JSON.stringify(config);
+
+		toggleDialog();
+		start(config);
+		console.log('done');
+	}
+});
 
 $j.get(chrome.extension.getURL('/modal.html'), function(data) {
-    // Or if you're using jQuery 1.8+:
 	$j($j.parseHTML(data)).appendTo('body');
-	/*$j('#getsumModalDialog').dialog({
-		'autoOpen' : false
-	});*/
+	$j('#closeGetsum').click(function() {
+		toggleDialog()
+	});
+	$j('#getsumModalDialog').hide();
 });
+function toggleDialog() {
+	$j('#getsumModalDialog').toggle("slide:right");
+}
+function start(configData){
+	data = JSON.stringify(configData);
+	console.log(data);
+	$j.post('http://127.0.0.1:8088',   // url
+		       { config: data }, // data to be submit
+		       function(data, status, jqXHR) {// success callback
+		                $j('#getsumModalDialog').append('status: ' + status + ', data: ' + data);
+		        })
+}
