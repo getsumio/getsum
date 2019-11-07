bd99d07e5ff517bd83a2b47af22fd2742d407941
burak <burak@linux.com>
2019-11-07 02:37:35 +0100

    firefox addon

---
+++ addons/firefox/getsum/background.js
@@ -0,0 +1,255 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var config = {
+	file : "",
+	algorithm : [],
+	checksum : "",
+	timeout : 60,
+	supplier : "go",
+	all: false,
+	remoteonly:false,
+	localonly:false,
+	timeout: 60,
+	key: "",
+	proxy: "",
+	insecureskipverify: true
+};
+
+var processId = "";
+const algs = [ "MD4", "MD5", "SHA1", "SHA224", "SHA256", "SHA384", "SHA512",
+		"RMD160", "SHA3-224", "SHA3-256", "SHA3-384", "SHA3-512", "SHA512-224",
+		"SHA512-256", "BLAKE2S256", "BLAKE2B256", "BLAKE2B384", "BLAKE2B512" ]
+
+var ids = []
+
+var options = JSON.parse(localStorage.config);
+config.supplier = options.lib;
+config.timeout = options.timeout;
+config.proxy = options.proxy
+console.log(options);
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+
+	if (tab) {
+		if (info.menuItemId.startsWith("validate")) {
+			config.file = info.linkUrl;
+			config.algorithm = [];
+			config.algorithm.push(info.menuItemId.split(':')[1]);
+			config.checksum = "";
+			console.log(config);
+			chrome.tabs.sendMessage(tab.id, {
+				"name" : "getsum",
+				"config" : config,
+				"ids" : tab.id
+			});
+		} else if (info.menuItemId.startsWith("calculate")) {
+			config.file = info.linkUrl;
+			config.algorithm = [];
+			config.algorithm.push(info.menuItemId.split(':')[1]);
+			config.checksum = "";
+			console.log(config);
+			chrome.tabs.sendMessage(tab.id, {
+				"name" : "getsum",
+				"config" : config,
+				"ids" : tab.id
+			});
+		}
+	}
+
+});
+chrome.contextMenus.removeAll(function() {
+	for (let i = 0; i < algs.length; i++) {
+		valId = 'validate:' + algs[i];
+		if (!ids.includes(valId)) {
+			chrome.contextMenus.create({
+				id : valId,
+				title : algs[i],
+				contexts : [ 'link' ]
+			});
+			ids.push(valId);
+		}
+	}
+});
+
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+
+function postToServer(dataStr, id){
+	processId = "";
+	console.log(options);
+	fetch(this.options.hostname, {
+	    method: 'post',
+	    cache: 'no-cache',
+	    headers: {
+	        'Accept': 'application/json',
+	        'Content-Type': 'application/json'
+	    },
+	    body: dataStr,
+	}) .then(handleErrors)
+	    .then(result => {
+	        // Here body is not ready yet, throw promise
+	        if (!result.ok) {
+	        	res.text().then(text => {throw Error(text);})
+	        }
+	        
+	        result.json().then(function (json) {
+	        	console.log("Post Response:");
+	        	console.log(json);
+	        	if(json.type == 4){
+	        		processId = json.value;
+		        	callBack(json, id);
+		        	listen(id);
+	        	}else{
+	        		callBack(json, id);
+	        	}
+	        });
+	    }).catch(error => {
+	    	 console.log("Get error:");
+			    errorStr = String(error);
+		       	console.log(errorStr);
+		       	if(errorStr.includes("Failed to fetch")){
+		       		errorStr = "Can not reach server @ " + options.hostname ;
+		       	}
+		       	callBack( {
+			    		type: 9,
+			    		value: errorStr,
+			    		checksum: ""
+			    	},id);
+	    });
+}
+
+function getFromServer(id){
+	if(processId == null || processId == ""){
+		return;
+	}
+	 return fetch(options.hostname + "/" + processId, {
+	    method: 'get',
+	    cache: 'no-cache',
+	    headers: {
+	        'Accept': 'application/json'
+	    }
+	}) .then(handleErrors)
+	    .then(result => {
+	        // Here body is not ready yet, throw promise
+	    	if (!result.ok) {
+	        	res.text().then(text => {throw Error(text);})
+	        }
+	        return result.json();
+	    }).catch(error => {
+	    	 console.log("Get error:");
+			    errorStr = String(error);
+		       	console.log(errorStr);
+		       	if(errorStr.includes("Failed to fetch")){
+		       		errorStr = "Can not reach server @ " + options.hostname ;
+		       	}
+		       	callBack( {
+			    		type: 9,
+			    		value: errorStr,
+			    		checksum: ""
+			    	},id);
+	    });
+}
+
+function terminate(id){
+	if(processId == null || processId == ""){
+		return;
+	}
+	 return fetch(options.hostname + "/"+ processId, {
+	    method: 'delete',
+	    cache: 'no-cache',
+	    headers: {
+	        'Accept': 'application/json'
+	    }
+	}) .then(handleErrors)
+	    .then(result => {
+	        // Here body is not ready yet, throw promise
+	    	if (!result.ok) {
+	        	res.text().then(text => {throw Error(text);})
+	        }
+	        processId = "";
+	    	callBack(  {
+	        	type: 13,
+	    		value: "Process terminated",
+	    		checksum: ""
+	        },id);
+	    }).catch(error => {
+		    console.log("Get error:");
+		    errorStr = String(error);
+		    processId = "";
+	       	console.log(errorStr);
+	       	if(errorStr.includes("Failed to fetch")){
+	       		errorStr = "Can not reach server @ " + options.hostname ;
+	       	}
+	       	callBack( {
+		    		type: 9,
+		    		value: errorStr,
+		    		checksum: ""
+		    	},id);
+	    });
+}
+
+
+function listen(id){
+	setTimeout(function () {  
+		getFromServer(id).then(result => {
+			console.log("Get feedback");
+			console.log(result);
+			if(result){
+				callBack(result, id);
+				if(result.type < 7){
+					listen(id);
+				}
+			}
+		});
+	   }, 1000)
+	
+}
+
+function callBack(result, id){
+	console.log('sending response');
+	console.log(result);
+	console.log(id);
+	if(result.type == 4){
+		result.value = "Waiting server response";
+	}
+	chrome.tabs.sendMessage(id, {
+		name : 'status',
+		dataStr : result,
+		id : id
+	});
+}
+
+
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+	if(options.hostname == ""){
+		callBack( {
+    		type: 9,
+    		value: "Please setup hostname from extension options",
+    		checksum: ""
+    	},id);
+		return;
+	}
+	if(message.requestType == 'start'){
+		console.log("Request received with id: " + message.ids);
+		if(processId != ""){
+			terminate(message.ids);
+			processId = "";
+		}
+		postToServer(message.dataStr,message.ids);
+		
+	}else if(message.requestType == 'terminate'){
+		console.log("Request received with id: " + message.ids);
+		terminate(message.ids);
+		
+	}
+});
+
+
+
---
+++ addons/firefox/getsum/content.js
@@ -0,0 +1,106 @@
+var $j = jQuery.noConflict();
+var pending = [];
+const statuses = [ "PREPARED", "ALLOCATED", "DOWNLOAD", "FETCHED", "STARTED",
+		"RESUMING", "RUNNING", "COMPLETED", "SUSPENDED", "ERROR", "TIMEDOUT",
+		"MISMATCH", "VALIDATED", "TERMINATED",
+
+];
+var validationChecksum = "";
+var tabId = "";
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	console.log('message received!');
+	console.log(request);
+	if (request.name == "getsum") {
+		config = request.config;
+		config.checksum = getSelectedText();
+		validationChecksum = config.checksum;
+		var cfg = JSON.stringify(config);
+		reset();
+		$j('#getsumModalDialog').show(200);
+		tabId = request.ids;
+		start(config, request.ids);
+	} else if (request.name == 'status') {
+		console.log(request.dataStr);
+		status = validate(request);
+		$j('#getsumLabel').text(status);
+		$j('#getsumValue').text(request.dataStr.value);
+		$j('#getsumChecksum').text(request.dataStr.checksum);
+	}
+});
+
+function getSelectedText(){
+	selection = window.getSelection().toString();
+	selection = selection.trim();
+	if(selection.includes(" ") || !isBase64(selection)){
+		return "";
+	}
+	return selection;
+}
+
+//https://stackoverflow.com/questions/7860392/determine-if-string-is-in-base64-using-javascript
+function isBase64(str) {
+    if (str ==='' || str.trim() ===''){ return false; }
+    try {
+        return btoa(atob(str)) == str;
+    } catch (err) {
+        return false;
+    }
+}
+
+function validate(request) {
+	if (request.dataStr.type >= 7) {
+		switch (request.dataStr.type) {
+		case 7:
+			return validateStatus(request);
+		case 12:
+			return validateStatus(request);
+		default:
+			return "\u24e7  " + statuses[request.dataStr.type];
+		}
+	}
+	return statuses[request.dataStr.type];
+}
+
+function validateStatus(request) {
+	if (validationChecksum != "") {
+		if (request.dataStr.checksum != validationChecksum) {
+			return '\u24e7  MISMATCH';
+		} else {
+			return '\u2713  VALIDATED';
+		}
+	} else {
+		return '\u2713  ' + statuses[request.dataStr.type];
+	}
+}
+
+$j.get(chrome.extension.getURL('/modal.html'), function(data) {
+	$j($j.parseHTML(data)).appendTo('body');
+	$j('#closeGetsum').click(function() {
+		reset();
+		chrome.runtime.sendMessage({
+			requestType : 'terminate',
+			ids : tabId
+		});
+		toggleDialog();
+	});
+	$j('#getsumModalDialog').hide();
+});
+function toggleDialog() {
+	$j('#getsumModalDialog').toggle("slide:right");
+}
+function reset() {
+	$j('#getsumLabel').text("");
+	$j('#getsumValue').text("");
+	$j('#getsumChecksum').text("");
+}
+function start(configData, id) {
+	data = JSON.stringify(configData);
+	console.log('starting calculation')
+	console.log(data);
+
+	chrome.runtime.sendMessage({
+		requestType : 'start',
+		dataStr : data,
+		ids : id
+	});
+}
