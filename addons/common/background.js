830c7134ae7dac760e4c1cc3e0f1770119c03d48
burak <burak@linux.com>
2019-11-09 04:55:51 +0100

    addons code cleanup comments

--- addons/common/background.js
+++ addons/common/background.js
@@ -2,6 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+//logger
+//https://stackoverflow.com/questions/18410119/is-it-possible-to-bind-a-date-time-to-a-console-log
+var DEBUG = (function(){
+    var timestamp = function(){};
+    timestamp.toString = function(){
+        return "[GetSum - " + (new Date).toLocaleTimeString() + "]:";    
+    };
+
+    return {
+        log: console.log.bind(console, '%s', timestamp)
+    }
+})();
+
+//base configration will be passed arround
 var config = {
 	file : "",
 	algorithm : [],
@@ -16,59 +30,50 @@ var config = {
 	proxy: "",
 	insecureskipverify: true
 };
+// default ops
 var ops = {
-		hostname : "",
-		proxy : "",
-		lib : "",
-		timeout: 180
-	}
+	hostname : "",
+	proxy : "",
+	lib : "",
+	timeout: 180
+}
+// current process id received from server on post
 var processId = "";
+// algos
 const algs = [ "MD4", "MD5", "SHA1", "SHA224", "SHA256", "SHA384", "SHA512",
 		"RMD160", "SHA3-224", "SHA3-256", "SHA3-384", "SHA3-512", "SHA512-224",
 		"SHA512-256", "BLAKE2S256", "BLAKE2B256", "BLAKE2B384", "BLAKE2B512" ]
-
-var ids = []
-var options = ops;
-if(localStorage.config && localStorage.config != null && localStorage.config != ""){
-	options = JSON.parse(localStorage.config);
-}
-
-config.supplier = options.lib;
-config.timeout = options.timeout;
-config.proxy = options.proxy
-console.log(options);
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-
-	if (tab) {
-		if (info.menuItemId.startsWith("validate")) {
-			config.file = info.linkUrl;
-			config.algorithm = [];
-			config.algorithm.push(info.menuItemId.split(':')[1]);
-			config.checksum = "";
-			console.log(config);
-			chrome.tabs.sendMessage(tab.id, {
-				"name" : "getsum",
-				"config" : config,
-				"ids" : tab.id
-			});
-		} else if (info.menuItemId.startsWith("calculate")) {
-			config.file = info.linkUrl;
-			config.algorithm = [];
-			config.algorithm.push(info.menuItemId.split(':')[1]);
-			config.checksum = "";
-			console.log(config);
-			chrome.tabs.sendMessage(tab.id, {
-				"name" : "getsum",
-				"config" : config,
-				"ids" : tab.id
-			});
+// tab id used by extension
+var ids = [];
+var options;
+setDefaults();
+DEBUG.log("Options parsed: ", options);
+/*
+ * Reads options Tries to parse if failure then uses default one
+ */
+function setDefaults(){
+	if(localStorage.config && localStorage.config != null && localStorage.config != ""){
+		try{
+			options = JSON.parse(localStorage.config);
+		}catch (e) {
+			DEBUG.log("Can not parse options!",e);
+			options = ops;
 		}
 	}
 
-});
+	config.supplier = options.lib;
+	config.timeout = options.timeout;
+	config.proxy = options.proxy
+}
+
+/*
+ * Add menu items
+ */
 chrome.contextMenus.removeAll(function() {
 	for (let i = 0; i < algs.length; i++) {
-		valId = 'validate:' + algs[i];
+		// make sure no duplicate
+		valId = 'getsum:' + algs[i];
+		DEBUG.log('Generating menu item: ' + valId);
 		if (!ids.includes(valId)) {
 			chrome.contextMenus.create({
 				id : valId,
@@ -79,18 +84,71 @@ chrome.contextMenus.removeAll(function() {
 		}
 	}
 });
+/*
+ * Add Context menu listener
+ */
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+	if (tab) {
+		if (info.menuItemId.startsWith("getsum")) {
+			
+			
+			config.file = info.linkUrl;
+			config.algorithm = [];
+			config.algorithm.push(info.menuItemId.split(':')[1]);
+			config.checksum = "";
+			DEBUG.log('Menu click: ' + info.menuItemId,config);
+			// send message on click to start process
+			// content.js will read selected text if exist
+			// and it will callback
+			chrome.tabs.sendMessage(tab.id, {
+				"name" : "getsum",
+				"config" : config,
+				"ids" : tab.id
+			});
+		} 
+	}
 
+});
+
+
+
+/*
+ * Checks response is not ok and throws response text
+ */
 function handleErrors(response) {
     if (!response.ok) {
-        throw Error(response.statusText);
+    	response.text().then(text => {throw Error(text);});
     }
     return response;
 }
 
-
+/*
+ * Validates if hostname is set otherwise callbacks with error code
+ */
+function checkOptions(){
+	if(!options || options.hostname == ""){
+		DEBUG.log('invalid option settings')
+		callBack( {
+    		type: 9, // ERROR
+    		value: 'Please first visit extension options and set hostname',
+    		checksum: ""
+    	},id); // tab id
+		return false;
+	}
+	return true;
+}
+/*
+ * Starts the process by posting config to server
+ */
 function postToServer(dataStr, id){
 	processId = "";
-	console.log(options);
+	
+	// validate options
+	if(!checkOptions()){
+		return;
+	}
+	
+	// post
 	fetch(this.options.hostname, {
 	    method: 'post',
 	    cache: 'no-cache',
@@ -99,133 +157,132 @@ function postToServer(dataStr, id){
 	        'Content-Type': 'application/json'
 	    },
 	    body: dataStr,
-	}) .then(handleErrors)
-	    .then(result => {
-	        // Here body is not ready yet, throw promise
-	        if (!result.ok) {
-	        	res.text().then(text => {throw Error(text);})
-	        }
-	        
-	        result.json().then(function (json) {
-	        	console.log("Post Response:");
-	        	console.log(json);
-	        	if(json.type == 4){
-	        		processId = json.value;
-		        	callBack(json, id);
-		        	listen(id);
-	        	}else{
-	        		callBack(json, id);
-	        	}
-	        });
-	    }).catch(error => {
-	    	 console.log("Get error:");
-			    errorStr = String(error);
-		       	console.log(errorStr);
-		       	if(errorStr.includes("Failed to fetch")){
-		       		errorStr = "Can not reach server @ " + options.hostname ;
-		       	}
-		       	callBack( {
-			    		type: 9,
-			    		value: errorStr,
-			    		checksum: ""
-			    	},id);
-	    });
+	}) 
+	// check response code
+	.then(handleErrors)
+	// parse response and call back
+	.then(result => {
+        result.json().then(function (json) {
+        	// 4 == STARTED
+        	DEBUG.log('Post response received', json);
+        	if(json.type == 4){
+        		// process id needed to watch status
+        		processId = json.value;
+	        	callBack(json, id);
+	        	listen(id);
+        	}else{
+        		// couldnt start return response
+        		callBack(json, id);
+        	}
+        });
+    })
+    // catch error
+	.catch(error => {
+		errorStr = getErrorString(error);
+       	callBackError(errorStr, id);
+	});
 }
 
+/*
+ * If processId present retrieves status from server
+ */
 function getFromServer(id){
-	if(processId == null || processId == ""){
+	// check options
+	if(!checkOptions() || processId == null || processId == ""){
 		return;
 	}
-	 return fetch(options.hostname + "/" + processId, {
+	// get
+	return fetch(options.hostname + "/" + processId, {
 	    method: 'get',
 	    cache: 'no-cache',
-	    headers: {
-	        'Accept': 'application/json'
-	    }
-	}) .then(handleErrors)
-	    .then(result => {
-	        // Here body is not ready yet, throw promise
-	    	if (!result.ok) {
-	        	res.text().then(text => {throw Error(text);})
-	        }
-	        return result.json();
-	    }).catch(error => {
-	    	 console.log("Get error:");
-			    errorStr = String(error);
-		       	console.log(errorStr);
-		       	if(errorStr.includes("Failed to fetch")){
-		       		errorStr = "Can not reach server @ " + options.hostname ;
-		       	}
-		       	callBack( {
-			    		type: 9,
-			    		value: errorStr,
-			    		checksum: ""
-			    	},id);
-	    });
+	}) 
+	// check response code
+	.then(handleErrors)
+	// return result
+	.then(result => {
+		DEBUG.log('Get response received', result);
+        return result.json();
+    }).catch(error => {
+    		errorStr = getErrorString(error);
+	       	callBackError(errorStr, id);
+    });
 }
 
 function terminate(id){
-	if(processId == null || processId == ""){
+	// check options
+	if(!checkOptions() || processId == null || processId == ""){
 		return;
 	}
+	// call delete
 	 return fetch(options.hostname + "/"+ processId, {
 	    method: 'delete',
 	    cache: 'no-cache',
-	    headers: {
-	        'Accept': 'application/json'
-	    }
-	}) .then(handleErrors)
-	    .then(result => {
-	        // Here body is not ready yet, throw promise
-	    	if (!result.ok) {
-	        	res.text().then(text => {throw Error(text);})
-	        }
-	        processId = "";
-	    	callBack(  {
-	        	type: 13,
-	    		value: "Process terminated",
-	    		checksum: ""
-	        },id);
-	    }).catch(error => {
-		    console.log("Get error:");
-		    errorStr = String(error);
-		    processId = "";
-	       	console.log(errorStr);
-	       	if(errorStr.includes("Failed to fetch")){
-	       		errorStr = "Can not reach server @ " + options.hostname ;
-	       	}
-	       	callBack( {
-		    		type: 9,
-		    		value: errorStr,
-		    		checksum: ""
-		    	},id);
-	    });
+	}) 
+	// check errors
+	.then(handleErrors)
+	// call back if ok
+    .then(result => {
+    	DEBUG.log('Terminate response received', result);
+        callBackFromParameters(id,13,"Process Terminated","");
+    }).catch(error => {
+    	errorStr = getErrorString(error);
+       	callBackError(errorStr, id);
+    });
+	// reset response id
+	processId = "";
 }
 
-
+/*
+ * Watch status until we got error
+ */
 function listen(id){
+	DEBUG.log("Starting listening");
 	setTimeout(function () {  
-		getFromServer(id).then(result => {
-			console.log("Get feedback");
-			console.log(result);
+		// get
+		getFromServer(id)
+		// parse return
+		.then(result => {
+			// we got result
 			if(result){
+				// notify content js
 				callBack(result, id);
+				// still running keep watching
 				if(result.type < 7){
 					listen(id);
 				}
 			}
 		});
-	   }, 1000)
+	   }, 1000);
 	
 }
 
+/*
+ * error to string
+ */
+function getErrorString(error){
+	if(!error){
+		return "";
+	}
+	
+	errorStr = String(error);
+   	if(errorStr.includes("Failed to fetch")){
+   		errorStr = "Can not reach server @ " + options.hostname ;
+   	}
+   	DEBUG.log(errorStr);
+   	return errorStr;
+}
+
+/*
+ * Notifies content.js
+ */
 function callBack(result, id){
-	console.log('sending response');
-	console.log(result);
-	console.log(id);
+	//4 == STARTED, add meaningful text
 	if(result.type == 4){
 		result.value = "Waiting server response";
 	}
+	
+	//callback
+	DEBUG.log("informing back content.js", result);
 	chrome.tabs.sendMessage(id, {
 		name : 'status',
 		dataStr : result,
@@ -233,29 +290,43 @@ function callBack(result, id){
 	});
 }
 
+/*
+ * utility to callback with json
+ */
+function callBackFromParameters(id,type,value,checksum){
+	callBack( {
+		type: type,
+		value: value,
+		checksum: checksum
+	},id);
+}
+
+/*
+ * utility to callback with error
+ */
+function callBackError(errorStr, id){
+	callBack( {
+		type: 9,
+		value: errorStr,
+		checksum: ""
+	},id);
+}
 
 
+/*
+ * When content.js sends message do related http request
+ */
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-	if(options.hostname == ""){
-		callBack( {
-    		type: 9,
-    		value: "Please setup hostname from extension options",
-    		checksum: ""
-    	},id);
-		return;
-	}
+	DEBUG.log('message received', message);
 	if(message.requestType == 'start'){
-		console.log("Request received with id: " + message.ids);
 		if(processId != ""){
+			DEBUG.log('There is an existing process cleaning|terminating ' + processId);
 			terminate(message.ids);
-			processId = "";
 		}
 		postToServer(message.dataStr,message.ids);
 		
 	}else if(message.requestType == 'terminate'){
-		console.log("Request received with id: " + message.ids);
 		terminate(message.ids);
-		
 	}
 });
 
